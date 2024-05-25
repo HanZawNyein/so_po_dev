db7b84d17e30d665adfb32837b597e305b9bbf03
Agga <agga@Aggas-MacBook-Air.local>
2024-05-25 22:47:48 +0700

    [auth] add network services for api

---
+++ lib/network/constants.dart
@@ -0,0 +1,8 @@
+// lib/constants.dart
+
+class Constants {
+  static const String apiHost = 'https://api.example.com';
+
+  // Endpoints
+  static const String loginEndpoint = '$apiHost/login';
+}
---
+++ lib/network/services.dart
@@ -0,0 +1,53 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'constants.dart';
+
+class ApiService {
+  Future<Map<String, dynamic>> requestLogin(
+      String username, String password) async {
+    final response = await http.post(
+      Uri.parse(Constants.loginEndpoint),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'username': username,
+        'password': password,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to login');
+    }
+  }
+
+  // General POST request
+  Future<Map<String, dynamic>> requestPost(Map<String, dynamic> data,
+      {String endpoint = Constants.loginEndpoint,
+      Map<String, String>? additionalHeaders}) async {
+
+    final headers = <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    };
+
+    if (additionalHeaders != null) {
+      headers.addAll(additionalHeaders);
+    }
+
+    final response = await http.post(
+      Uri.parse(endpoint),
+      headers: headers,
+      body: jsonEncode(data),
+    );
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception(
+          'Failed to fetch data. Status code: ${response.statusCode}');
+    }
+  }
+}
