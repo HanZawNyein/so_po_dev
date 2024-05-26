c077a5af6b88b0863b37097b04141c2066eedf9b
Agga <agga@Aggas-MacBook-Air.local>
2024-05-26 15:44:10 +0700

    [apps][home_screen] sample grid view

--- lib/apps/screens/home_scree.dart
+++ lib/apps/screens/home_scree.dart
@@ -5,8 +5,38 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<String> items = List<String>.generate(20, (index) => "Item $index");
     return Scaffold(
       appBar: AppBar(title: Text("HOME"),),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, // Number of columns
+            crossAxisSpacing: 10.0, // Horizontal space between grid items
+            mainAxisSpacing: 10.0, // Vertical space between grid items
+          ),
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            // return GridItem(item: items[index]);
+            return Container(
+              decoration: BoxDecoration(
+                color: Colors.blueAccent,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Center(
+                child: Text(
+                  items[index],
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
